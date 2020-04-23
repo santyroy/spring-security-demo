27888db35695ba3489e275f606aaca5a00a20a67
sougata <sougata.roy26@yahoo.com>
2020-04-23 18:29:23 +0530

    Un-Secure app

---
+++ src/main/java/org/roy/unsecureapp/UnsecureAppApplication.java
@@ -0,0 +1,13 @@
+package org.roy.unsecureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnsecureAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnsecureAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/roy/unsecureapp/UnsecureAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.roy.unsecureapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnsecureAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
